a0afa2381da28edeed9d9dd3aa64546fbe8ace7b
Xiaosong-zhai <115197218+xiaosong-zhai@users.noreply.github.com>
2023-11-08 12:33:20 -0800

    add log file and build ami for a7

--- src/main/java/northeastern/xiaosongzhai/csye6225/aspect/StatsDAspect.java
+++ src/main/java/northeastern/xiaosongzhai/csye6225/aspect/StatsDAspect.java
@@ -1,10 +1,14 @@
 package northeastern.xiaosongzhai.csye6225.aspect;
 
 import com.timgroup.statsd.StatsDClient;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Author: Xiaosong Zhai
@@ -14,6 +18,10 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class StatsDAspect {
+
+    private final ConcurrentHashMap<String, Integer> apiCallCounts = new ConcurrentHashMap<>();
+
+    private static final Logger logger = LogManager.getLogger(StatsDAspect.class);
 
     private final StatsDClient statsDClient;
 
@@ -25,5 +33,8 @@ public class StatsDAspect {
     public void beforeControllerMethod(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         statsDClient.incrementCounter(methodName + ".called");
+
+        int currentCount = apiCallCounts.compute(methodName, (key, val) -> (val == null) ? 1 : val + 1);
+        logger.info("API Method " + methodName + " called, count: " + currentCount);
     }
 }
